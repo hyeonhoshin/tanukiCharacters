75e9ddb615e7935cd365f8092fe3c62e6b996b8b
Tanukimong <35714982+Tanukimong@users.noreply.github.com>
2020-12-08 22:46:31 +0900

    Typo 수정

--- tanukiChar_trainer_with_gen_aug.py
+++ tanukiChar_trainer_with_gen_aug.py
@@ -112,7 +112,7 @@ for epoch in range(epochs):
         optimizer.step()
 
         _, predicted = outputs.max(1)
-        total += targets.size(0)
+        total += labels.size(0)
         correct += predicted.eq(targets).sum().item()
 
         # print statistics
